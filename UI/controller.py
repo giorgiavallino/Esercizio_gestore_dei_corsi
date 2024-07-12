1aeca125dd328fde273a18244f5ea37d231848b9
s298157 <s298157@studenti.polito.it>
2024-07-12 12:10:05 +0200

    Esercizio_gestore_dei_corsi

--- UI/controller.py
+++ UI/controller.py
@@ -8,10 +8,14 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
-            return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
-        self._view.update_page()
+    def get_corsi_periodo(self):
+        pass
+
+    def get_studenti_periodo(self):
+        pass
+
+    def get_studenti_corso(self):
+        pass
+
+    def get_dettaglio_corso(self):
+        pass
--- UI/view.py
+++ UI/view.py
@@ -1,46 +1,69 @@
 import flet as ft
 
-
 class View(ft.UserControl):
     def __init__(self, page: ft.Page):
         super().__init__()
-        # page stuff
+        # Elementi relativi alla pagina in generale
         self._page = page
         self._page.title = "Template application using MVC and DAO"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
-        # controller (it is not initialized. Must be initialized in the main, after the controller is created)
+        self._page.theme_mode = ft.ThemeMode.LIGHT
+        # Controller
         self._controller = None
-        # graphical elements
+        # Elementi grafici
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
-        self.txt_result = None
-        self.txt_container = None
+        self.dd_pd = None
+        self.btn_corsi_periodo = None
+        self.btn_studenti_periodo = None
+        self.txt_codice_corso = None
+        self.btn_studenti_corso = None
+        self.btn_dettaglio_corso = None
+        self.lv_result = None
 
     def load_interface(self):
-        # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
-        self._page.controls.append(self._title)
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
+        # Titolo
+        self._title = ft.Text("Gestore dei corsi", color="blue", size=24)
+        self._page.add(self._title)
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
-        self._page.controls.append(row1)
+        # Row 1
+        self.dd_pd = ft.Dropdown(label = "Periodo didattico",
+                                 hint_text = "Selezionare il periodo didattico",
+                                 options = [ft.dropdown.Option(key = "1"), ft.dropdown.Option(key = "2")],
+                                 width = 200)
+        self.btn_corsi_periodo = ft.ElevatedButton(text = "Corsi del periodo didattico",
+                                                   tooltip = "Ottenere i corsi del periodo didattico selezionato",
+                                                   on_click = self._controller.get_corsi_periodo)
+        self.btn_studenti_periodo = ft.ElevatedButton(text = "Studenti del periodo didattico",
+                                                      tooltip = "Ottenere gli studenti iscritti ai corsi del periodo didattico",
+                                                      on_click = self._controller.get_studenti_periodo)
+        row_01 = ft.Row([self.dd_pd, self.btn_corsi_periodo, self.btn_studenti_periodo],
+                        alignment = ft.MainAxisAlignment.CENTER)
+        self._page.add(row_01)
+        self.update_page()
 
-        # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-        self._page.controls.append(self.txt_result)
-        self._page.update()
+        # Row 2
+        self.txt_codice_corso = ft.TextField(label = "Codice del corso",
+                                             hint_text = "Inserire il codice del corso",
+                                             width = 400)
+        self.btn_studenti_corso = ft.ElevatedButton(text = "Studenti del corso",
+                                                    tooltip = "Ottenere gli studenti iscritti al corso selezionato",
+                                                    on_click = self._controller.get_studenti_corso)
+        self.btn_dettaglio_corso = ft.ElevatedButton(text = "Corso di studio degli studenti",
+                                                     tooltip = "Ottenere i corsi di studio degli iscritti al corso selezionato",
+                                                     on_click = self._controller.get_dettaglio_corso)
+        row_02 = ft.Row([self.txt_codice_corso, self.btn_studenti_corso, self.btn_dettaglio_corso],
+                        alignment = ft.MainAxisAlignment.CENTER)
+        self._page.add(row_02)
+        self.update_page()
+
+        # List view --> è un contenitore di righe, non un campo di testo!
+        self.lv_result = ft.ListView(expand = 1,
+                                     spacing = 10,
+                                     padding = 20,
+                                     auto_scroll = True)
+        self._page.add(self.lv_result)
+        self.update_page()
 
     @property
     def controller(self):
